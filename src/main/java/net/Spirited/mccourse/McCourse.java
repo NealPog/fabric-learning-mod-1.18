a01e23ce26da6318757e63e6b5fe3f115ed2c59a
Spirited <nealsaraswat@gmail.com>
2024-03-12 14:51:46 +0530

    test

---
+++ src/main/java/net/Spirited/mccourse/McCourse.java
@@ -0,0 +1,18 @@
+package net.Spirited.mccourse;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class McCourse implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
